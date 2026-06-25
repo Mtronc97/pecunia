2e25f2c8237b498a0b14d964847baa05c9ba3b00
Mtronc97 <mtronc97@gmail.com>
2026-06-25 15:22:06 -0300

    agregamos express con root endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Pecunia: el entorno de TypeScript funciona correctamente");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("¡Bienvenido a la API de Pecunia! 🪙");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
