3c46251cf41746ea015750c559fb633a47bd625a
Mtronc97 <mtronc97@gmail.com>
2026-06-29 14:49:53 -0300

    feat: integración de routes y json middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
+import transactionRoutes from "./routes/transaction.routes.js";
 import express from "express";
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("¡Bienvenido a la API de Pecunia! 🪙");
 });
+
+app.use("/transactions", transactionRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
