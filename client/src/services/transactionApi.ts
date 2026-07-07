ce5756d7589bd7be45a964de0439746cdcfa2e96
Mtronc97 <mtronc97@gmail.com>
2026-07-07 18:23:29 -0300

    fix: variable API_URL

--- client/src/services/transactionApi.ts
+++ client/src/services/transactionApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { Transaction } from "../types/transaction";
 
-const API_URL = "http://localhost:3000/transactions";
+const API_URL = `${import.meta.env.VITE_API_URL || "http://localhost:3000"}/transactions`;
 
 export async function fetchTransactions(): Promise<Transaction[]> {
   const response = await axios.get<Transaction[]>(API_URL);
